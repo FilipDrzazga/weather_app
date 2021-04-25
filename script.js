ef3183fcf28d99ac8b74db1d76c99bc67a95a946
Filip <43039909+Urfisior@users.noreply.github.com>
2021-04-25 12:11:50 +0100

    add new method to app currentWeatherAPI

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class App {
     constructor() {
         this.date = new Date();
     }
-    // -get user position from web api
+    // -get geolocation from web api
     userGeolocationAPI() {
        const userGeoPosition =  new Promise(function (resolve, reject) {
             return navigator.geolocation.getCurrentPosition(resolve, reject)
@@ -15,23 +15,39 @@ class App {
     userCurrentCity() {
         const getUserCity = async () => {
             try {
-
                 const latitudeAndLongitude = await this.userGeolocationAPI();
                 const { latitude: lat, longitude: lon } = latitudeAndLongitude.coords;
 
                 const cityNameResponse = await fetch(`https://geocode.xyz/${lat},${lon}?geoit=json`);
                 if (!cityNameResponse.ok) throw new Error('Something wrong with getting your location');
                 const cityNameData = await cityNameResponse.json();
-                return cityNameData.city
+                return cityNameData.city;
 
             } catch (err) {
                 throw new Error(err.message);
             };
         }
-        return getUserCity().then(res => { console.log(res); }).catch(err => { console.log(err.message = `Something wrong with getting your location`)});
+        return getUserCity()
+            .then(res => res)
+            .catch(err => {
+                console.log(err.message,'Something wrong with getting your location')
+            });
 }
     // get data from weather api
-    currentWeatherAPI() {
+    currentWeatherAPI(city) {
+        const getActyallyWeather = async () => {
+            try {
+                const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weathker?q=${city}&appid=75c417a54f0800d4e732c58c7f6427eb`);
+                if (!weatherResponse.ok) throw new Error('Something wrong with getting api weather!')
+                const weatherData = await weatherResponse.json();
+                return weatherData;
+            } catch(err) {
+                throw new Error(err.message);
+            };
+        };
+        return getActyallyWeather()
+            .then(res => console.log(res))
+            .catch(err=> console.log(err.message, 'Something wrong with getting weather!'));
 
     };
     // create all html on currentWeather() method display all content
@@ -48,6 +64,10 @@ class App {
     };
     // remove city from the list (remove from all HTML)
     removeCityFromList() {
+
+    };
+    // render any Error
+    renderErr() {
 
     };
 
