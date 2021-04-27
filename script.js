8db08a99492efe931cd97ebcab004c2fb1855dc2
Filip <43039909+Urfisior@users.noreply.github.com>
2021-04-27 20:57:16 +0100

    current weather from API (based on userCurrentCity method) and added to the constructor func. + displayWeather method

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
 'use strict'
+
+const introSection = document.querySelector('.intro');
 class App {
     constructor() {
         this.date = new Date();
-    }
+        this.weatherData = '';
+        this.userData = {
+            latitude: '',
+            longitude: '',
+            city: '',
+        };
+        this.userCurrentCity();
+
+    };
     // -get geolocation from web api
     userGeolocationAPI() {
        const userGeoPosition =  new Promise(function (resolve, reject) {
-            return navigator.geolocation.getCurrentPosition(resolve, reject)
+           return navigator.geolocation.getCurrentPosition(resolve, reject);
        });
-        return userGeoPosition
+        return userGeoPosition.then(res => {
+            let { latitude, longitude } = res.coords;
+            this.userData.latitude = latitude;
+            this.userData.longitude = longitude;
+        }).catch(err=>console.log(err.message));
     };
 
-    // get current user city name
+    // get current user city
     userCurrentCity() {
         const getUserCity = async () => {
             try {
-                const latitudeAndLongitude = await this.userGeolocationAPI();
-                const { latitude: lat, longitude: lon } = latitudeAndLongitude.coords;
-
-                const cityNameResponse = await fetch(`https://geocode.xyz/${lat},${lon}?geoit=json`);
+                await this.userGeolocationAPI();
+                const cityNameResponse = await fetch(`https://geocode.xyz/${this.userData.latitude},${this.userData.longitude}?geoit=json`);
                 if (!cityNameResponse.ok) throw new Error('Something wrong with getting your location');
                 const cityNameData = await cityNameResponse.json();
-                return cityNameData.city;
+                this.userData.city = cityNameData.city;
+                return this.currentWeatherAPI(cityNameData.city);
 
             } catch (err) {
                 throw new Error(err.message);
             };
-        }
+        };
         return getUserCity()
             .then(res => res)
-            .catch(err => {
-                console.log(err.message,'Something wrong with getting your location')
-            });
-}
+            .catch(err => {console.log(err.message,'Something wrong with getting your location')});
+    };
     // get data from weather api
     currentWeatherAPI(city) {
         const getActyallyWeather = async () => {
             try {
-                const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weathker?q=${city}&appid=75c417a54f0800d4e732c58c7f6427eb`);
-                if (!weatherResponse.ok) throw new Error('Something wrong with getting api weather!')
+                const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&&units=metric&appid=a938e8c556fa1ec0d334507134f03c40`);
+                if (!weatherResponse.ok) throw new Error('Something wrong with getting api weather!');
                 const weatherData = await weatherResponse.json();
                 return weatherData;
             } catch(err) {
@@ -46,13 +57,40 @@ class App {
             };
         };
         return getActyallyWeather()
-            .then(res => console.log(res))
+            .then(res => this.renderData(res))
             .catch(err=> console.log(err.message, 'Something wrong with getting weather!'));
 
     };
+    // take data from promise and take to the class
+    renderData(data) {
+        this.weatherData = data;
+    };
     // create all html on currentWeather() method display all content
     displayWeather() {
+        const markup = `
+        <h1 class="main__location">${this.weatherData.name}, ${this.weatherData.sys.country}</h1>
+        <button class="main__add-location"><i class="fas fa-plus"></i></button>
+        <p class="main__date">Monday, 23 March</p>
+        <p class="main__location-time">15:32</p>
+        <div class="main__weather-location">
+            <svg class="sunny" viewBox="0 0 118 118">
+                <circle cx="59" cy="59" r="59" fill="#f8ab1c" /></svg>
+        </div>
+        <p class="main__weather-description">${this.weatherData.weather[0].description}</p>
+        <p class="main__weather-temp">${+(this.weatherData.main.temp).toFixed(1)}°C</p>
+        <div class="main__swiper-container swiper-container">
+            <div class="main__swiper-wrapper swiper-wrapper">
+                <div class="main__swiper-slide swiper-slide">
+                    <p class="main__title">Wind</p>
+                    <p class="main__value">${this.weatherData.wind.speed}km/h</p>
+                </div>
+            </div>
+        </div>`;
 
+        const htmlEl = document.createElement('section');
+        htmlEl.innerHTML = markup;
+        htmlEl.classList.add('main');
+        introSection.insertAdjacentElement('afterend', htmlEl);
     };
     // search cities by input
     searchCity() {
@@ -72,8 +110,7 @@ class App {
     };
 
 };
-const app = new App()
-// https: //geocode.xyz/lat,lot?geoit=json
+const app = new App();
 
 
 
